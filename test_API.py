7a800477b21c8c5ad24155f821423e506ee7ad4f
Insia <insia.resistance@gmail.com>
2023-10-26 15:30:32 +0200

    test open repo

---
+++ test_API.py
@@ -0,0 +1,20 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Model.list()
+
+# Example OpenAI Python library request
+MODEL = "gpt-3.5-turbo"
+response = openai.ChatCompletion.create(
+    model=MODEL,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Knock knock."},
+        {"role": "assistant", "content": "Who's there?"},
+        {"role": "user", "content": "Orange."},
+    ],
+    temperature=0,
+)
+
+print(response)
