0a64ee00cc3697c71119b881991dbac1573d63df
insiares <Insia.resistance@gmail.com>
2023-10-29 15:26:21 +0100

    update call api

--- Prototype_NLT/Components/Call_API.py
+++ Prototype_NLT/Components/Call_API.py
@@ -3,17 +3,29 @@ import os
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def Call_GPT(prompt) : 
+def call_gpt(prompt, langage = 'python', style ='code') : 
+    """
+    This function calls the GPT-3.5-turbo model from OpenAI to generate a response.
+    It takes a prompt as input and returns the generated response.
+    """
+    langage_dict = { 'python' : 'You are a Python senior programmer.',
+                      'JS' : 'You are a JavaScript senior programmer.',
+                      'C' : 'You are a C senior programmer.'}
+    style_dict = { 'code' : '''Respond only with none formatted code. 
+                  Do not write text outside the code block.
+                  No example usage.
+                  No output example.
+                  if something absolutely needs clarification, do so as a comment inside the code.''',
+                    'pedago' : ''' Please respond with explanations inside the code.
+                                Help me understand with comments.
+                    Only code or comments within a single code block in markdown are allowed.
+         Do not write text outside the code block.'''}
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
     messages=[
         {"role": "system", "content": 
-        '''Tu es un assistant de programmation en python 3, tu réponds de manière succinte. 
-        je ne veux en retour que du code ou des commentaires, à l'intérieur d'un unique bloc de code en markdown.
-        Il est interdit d'écrire du texte en dehors du bloc de code.
-        '''},
-        {"role": "user", "content": '''donne moi une fonction d'addition'''},
-        {"role": "assistant", "content":" ```def addition(a, b): \n return a + b```"},        
+        f'''{langage_dict[langage]} {style_dict[style]}
+        '''},        
         {"role": "user", "content": prompt}],
     temperature=0,
     max_tokens=100,
