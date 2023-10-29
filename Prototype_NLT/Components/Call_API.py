1e858fe27f67cc954742ba0f1312ece9d83e85fb
Insiares <133666385+Insiares@users.noreply.github.com>
2023-10-29 15:27:15 +0100

    Update Call_API.py
    second update for formatting

--- Prototype_NLT/Components/Call_API.py
+++ Prototype_NLT/Components/Call_API.py
@@ -1,6 +1,5 @@
 import openai
 import os
-import streamlit as st
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -11,21 +10,19 @@ def call_gpt(prompt, langage = 'python', style ='code'):
     WIP : optional arguments to change coding langage and output style (pure code, or pegadogic output)
     """
     # Define the messages for the chat completion
-    langage_dict = { 
-        'python' : 'You are a Python programming assistant.',
-        'JS' : 'You are a JavaScript programming assistant.',
-        'C' : 'You are a C programming assistant.'
-    }
+    langage_dict = { 'python' : 'You are a Python senior programmer.',
+                      'JS' : 'You are a JavaScript senior programmer.',
+                      'C' : 'You are a C senior programmer.'}
+    style_dict = { 'code' : '''Respond only with none formatted code. 
+                  Do not write text outside the code block.
+                  No example usage.
+                  No output example.
+                  if something absolutely needs clarification, do so as a comment inside the code.''',
+                    'pedago' : ''' Please respond with explanations inside the code.
+                                Help me understand with comments.
+                    Only code or comments within a single code block in markdown are allowed.
+         Do not write text outside the code block.'''}
     
-    style_dict = { 
-        'code' : '''Please respond succinctly. 
-         Only code or comments within a single code block in markdown are allowed.
-         Do not write text outside the code block.''',
-         'pedago' : ''' Please respond with explanations inside the code.
-        Help me understand with comments.
-        Only code or comments within a single code block in markdown are allowed.
-        Do not write text outside the code block.'''
-                 }
     messages=[
         {"role": "system", "content": 
         f'''{langage_dict[langage]} {style_dict[style]}
